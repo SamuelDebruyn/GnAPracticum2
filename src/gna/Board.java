c840ddce464ab3c2926913107c8602e06abc720c
SamuelDebruyn <s@muel.be>
2013-03-25 13:58:33 +0100

    import en eerste commit in board

---
+++ src/gna/Board.java
@@ -0,0 +1,47 @@
+package gna;
+
+public class Board
+{
+	
+	private final int[][] tiles;
+	private final int height;
+	private final int width;
+	
+	// construct a board from an N-by-N array of tiles
+	public Board( int[][] tiles )
+	{
+		this.tiles = tiles;
+		this.width = tiles.length;
+		this.height = tiles[0].length;
+	}
+	
+	// return number of blocks out of place
+	public int hamming()
+	{
+		return 0;
+	}
+	
+	// return sum of Manhattan distances between blocks and goal
+	public int manhattan()
+	{
+		return 0;
+	}
+	
+	// does this board position equal y
+	public boolean equals(Object y)
+	{
+		return false;
+	}
+	
+	// return an Iterable of all neighboring board positions
+	public Iterable<Board> neighbors()
+	{
+		return null;
+	}
+	
+	// return a string representation of the board
+	public String toString()
+	{
+		return "<empty>";
+	}
+}
---
+++ src/gna/SolverTest.java
@@ -0,0 +1,16 @@
+package gna;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/**
+ * A number of JUnit tests for Solver.
+ *
+ * Feel free to modify these test to check additional properties.
+ */
+public class SolverTest {
+  
+  @Test
+  public void test() {
+  }
+}
