bef5f588f1e17560a1b9376c62653f88987f6f9a
SamuelDebruyn <s@muel.be>
2013-03-25 15:41:53 +0100

    found corners & hulpmethodes

--- src/gna/Board.java
+++ src/gna/Board.java
@@ -1,6 +1,6 @@
 package gna;
 
-import java.util.Iterator;
+import java.util.ArrayList;
 
 public class Board
 {
@@ -17,6 +17,27 @@ public class Board
 		this.height = tiles[0].length;
 	}
 	
+	// returns the array with the tiles
+	public int[][] getTiles() {
+		return tiles;
+	}
+	
+	// returns the height
+	public int getHeight() {
+		return height;
+	}
+
+	// returns the width
+	public int getWidth() {
+		return width;
+	}
+	
+	// return the tile with the given indexes
+	public int getTile(int i, int j){
+		int[][] tiles = this.getTiles();
+		return tiles[i][j];
+	}
+
 	// return number of blocks out of place
 	public int hamming()
 	{
@@ -38,20 +59,50 @@ public class Board
 	// return an Iterable of all neighboring board positions
 	public Iterable<Board> neighbors()
 	{
-		return new Iterable<Board>(){
-
-			@Override
-			public Iterator<Board> iterator() {
-				// TODO Auto-generated method stub
-				return null;
-			}
+		ArrayList<Board> neighborList = new ArrayList<Board>();
+		int[] emptyIndexes = this.getEmpty();
+		
+		//left upper corner
+		if(emptyIndexes[0] == 0 && emptyIndexes[1] == 0){
 			
-		};
+		//right upper corner
+		}else if(emptyIndexes[0] == 0 && emptyIndexes[1] == this.getWidth() - 1){
+			
+		//left bottom corner
+		}else if(emptyIndexes[1] == 0 && emptyIndexes[0] == this.getHeight() - 1){
+			
+		//right bottom corner
+		}else if(emptyIndexes[1] == this.getWidth() - 1 && emptyIndexes[0] == this.getHeight() - 1){
+			
+		//not a corner
+		}else{
+			
+		}
+		
+		return neighborList;
+		
 	}
 	
 	// return a string representation of the board
 	public String toString()
 	{
 		return "<empty>";
+	}
+	
+	// return the indexes with the empty position
+	public int[] getEmpty(){
+		int[] result = new int[2];
+		result[0] = 0;
+		result[1] = 0;
+		for(int i=0; i<this.getHeight(); i++){
+			for(int j=0; j<this.getWidth(); i++){
+				if(this.getTile(i, j) == 0){
+					result[0] = i;
+					result[1] = j;
+					return result;
+				}
+			}
+		}
+		return result;
 	}
 }
