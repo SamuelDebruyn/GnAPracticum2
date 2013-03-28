9bb275385bde5e07ad6e19f71e7c8b589977bb04
SamuelDebruyn <github.s@muel.be>
2013-03-28 14:53:43 +0100

    permutation isSolvable

--- src/gna/Solver.java
+++ src/gna/Solver.java
@@ -1,5 +1,7 @@
 package gna;
 
+import java.util.ArrayList;
+
 public class Solver
 {
 	
@@ -21,9 +23,37 @@ public class Solver
 	{
 		// first move the empty tile to its correct position
 		int[] emptyCoords = this.getInitial().getEmpty();
-		int width = this.getInitial().getSize();
+		int x = emptyCoords[1];
+		int y = emptyCoords[0];
+		int size = this.getInitial().getSize();
+		int[][] tiles = this.getInitial().getTiles();
+		boolean isSolvable = false;
 		
-		return false;
+		while(x != size -1){
+			int old = tiles[y][x + 1];
+			tiles[y][x + 1] = 0;
+			tiles[y][x] = old;
+			x++;
+		}
+		
+		while(y != size -1){
+			int old = tiles[y + 1][x];
+			tiles[y + 1][x] = 0;
+			tiles[y][x] = old;
+			y++;
+		}
+		
+		ArrayList<Integer> permutation = new ArrayList<Integer>();
+		
+		for(int i = 0; i < size; i++){
+			for(int j = 0; j < size; j++){
+				if(i != size - 1 && j != size - 1){
+					permutation.add(tiles[i][j]);
+				}
+			}
+		}
+		
+		return isSolvable;
 	}
 	
 	// return min number of moves to solve initial board;
